f9c1e85f4d37b959bff8423829226924c5dc7020
unknown <shivanshi.gupta22@gmail.com>
2021-07-22 15:57:59 +0530

    First commit

---
+++ server/index.js
@@ -0,0 +1,61 @@
+const express= require('express')
+const mongoose = require('mongoose')
+const FoodModel= require('./models/Food')
+const cors = require('cors')
+const app = express()
+//const port = process.env.PORT || 3000
+
+app.use(express.json())
+app.use(cors())
+
+// mongoose.connect('mongodb+srv://Shivanshi:Shivanshi@crud.loejr.mongodb.net/food?retryWrites=true&w=majority', {
+//     useNewUrlParser: true
+// })
+
+mongoose.connect('mongodb://127.0.0.1:27017/food', {useCreateIndex: true, useNewUrlParser:true, useUnifiedTopology:true})
+
+app.post('/insert', async (req, res) => {
+    const foodName = req.body.foodName
+    const days = req.body.days
+    
+    const food = new FoodModel({foodName: foodName, daysSinceIAte: days})
+    try{
+        await food.save()
+        console.log(food)
+    }catch(err){
+        res.send(err)
+    }
+})
+
+app.get('/read', async (req, res) => {
+    //FoodModel.find({ $where : {foodName: "Apple"}})
+    FoodModel.find({}, (err, result) => {
+        if(err) res.send(err)
+        res.send(result)
+    })
+})
+
+app.put('/update', async (req, res) => {
+    const newFoodName = req.body.newFoodName
+    const id = req.body.id
+
+    try{
+        await FoodModel.findById(id, (err, updatedFood) => {
+            updatedFood.foodName = newFoodName
+            updatedFood.save()
+            res.send("update")
+        })
+    }catch(err){
+        res.send(err)
+    }
+})
+
+app.delete('/delete/:id', async (req, res) => {
+    const id= req.params.id
+    await FoodModel.findByIdAndRemove(id).exec()
+    res.send("deleted")
+})
+
+app.listen(3001, () => {
+    console.log('Server is running on port 3001...')
+})
